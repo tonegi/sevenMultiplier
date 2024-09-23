3ffe6f2a71495382a292c3a95eae242f21e62087
tonegi <90652377+tonegi@users.noreply.github.com>
2024-09-23 15:55:59 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+int wholeNumber, product;
+printf("Type a whole number: ");
+  scanf("%d",&wholeNumber); 
+product = wholeNumber * 7;  
+printf("This number multiplied by 7 is: %d\n", product);  
+return 0;
+}
